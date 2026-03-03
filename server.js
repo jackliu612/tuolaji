f9606f178b41b2310dec79811e43e6603c098470
Jack Liu <jackliu612@gmail.com>
2026-03-03 21:54:21 +0000

    first upload

---
+++ server.js
@@ -0,0 +1,21 @@
+import { createServer } from 'node:http';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+server.listen(8001, () => {
+    console.log('Server running at http://localhost:8001');
+});
